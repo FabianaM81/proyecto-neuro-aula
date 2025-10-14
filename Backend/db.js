932a3b3cea3b36f643126db7ccb24599c0b37aa6
´FabianaM81´ <brefamago@gmail.com>
2025-10-14 12:04:12 -0500

    Limpieza de archivos duplicados en la raíz del proyecto

--- Backend/db.js
+++ Backend/db.js
@@ -4,7 +4,7 @@ const conexion = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3307,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'admin',
+  password: process.env.DB_PASSWORD || '1234',
   database: process.env.DB_NAME || 'proyecto',
 });
 
